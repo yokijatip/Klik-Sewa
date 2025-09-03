016710e5b5f3ad7ebe0f42265576b7e61493650c
Yoki Jati Perkasa <yokijati@gmail.com>
2025-09-03 22:02:59 +0700

    Sudah berhasil conflict ksp

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
         jvmTarget = "11"
@@ -47,12 +47,16 @@ android {
     }
     packaging {
         resources {
-            pickFirsts += ("/META-INF/versions/9/OSGI-INF/MANIFEST.MF")
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            pickFirsts += "/META-INF/versions/9/OSGI-INF/MANIFEST.MF"
         }
     }
 }
 
 dependencies {
+    // TAMBAHKAN HILT DEPENDENCIES - INI PENTING!
+    implementation("com.google.dagger:hilt-android:2.51.1")
+    ksp("com.google.dagger:hilt-compiler:2.51.1")
 
     // Logging
     implementation(libs.timber)
@@ -67,27 +71,19 @@ dependencies {
     implementation(libs.lifecycle.livedata.ktx)
     implementation(libs.androidx.room.runtime)
     ksp(libs.room.compiler)
-
-    // Dagger hilt
-    //noinspection UseTomlInstead
-    implementation("com.google.dagger:hilt-android:2.51.1")
-    //noinspection UseTomlInstead
-    ksp("com.google.dagger:hilt-android-compiler:2.51.1")
+    // TAMBAHKAN ROOM KTX
+    implementation("androidx.room:room-ktx:2.6.1")
 
     // Coil untuk loading gambar
-    //noinspection UseTomlInstead
     implementation("io.coil-kt:coil:2.5.0")
-    //noinspection UseTomlInstead
-    implementation("io.coil-kt.coil3:coil-compose:3.0.4")
-    //noinspection UseTomlInstead
-    implementation("io.coil-kt.coil3:coil-network-okhttp:3.0.4")
+    // HAPUS COIL3 YANG KONFLIK
+    // implementation("io.coil-kt.coil3:coil-compose:3.0.4")
+    // implementation("io.coil-kt.coil3:coil-network-okhttp:3.0.4")
 
     // Paging 3
-    //noinspection UseTomlInstead
     implementation("androidx.paging:paging-runtime-ktx:3.3.5")
 
     //  Kebutuhan untuk menyimpan token yang aman
-    //noinspection UseTomlInstead
     implementation("androidx.security:security-crypto:1.1.0-alpha06")
 
     //  Delegate ViewModel
@@ -95,32 +91,26 @@ dependencies {
     implementation(libs.androidx.navigation.ui.ktx)
 
     //  Chip Navigation Bar
-    //noinspection UseTomlInstead
     implementation("com.github.ismaeldivita:chip-navigation-bar:1.4.0")
 
     // Import the Firebase BoM
-    //noinspection UseTomlInstead
     implementation(platform("com.google.firebase:firebase-bom:33.8.0"))
 
     // When using the BoM, don't specify versions in Firebase dependencies
-    //noinspection UseTomlInstead
     implementation("com.google.firebase:firebase-analytics")
 
     // Image Auto Slider
-    //noinspection UseTomlInstead
     implementation("com.github.denzcoskun:ImageSlideshow:0.1.2")
 
     // Circle Image
-    //noinspection UseTomlInstead
     implementation("de.hdodenhof:circleimageview:3.1.0")
 
     // Cloudinary
-    //noinspection UseTomlInstead
-    implementation ("com.cloudinary:cloudinary-android:2.3.1")
+    implementation("com.cloudinary:cloudinary-android:2.3.1")
 
     // Glide
-    //noinspection UseTomlInstead
     implementation("com.github.bumptech.glide:glide:4.16.0")
+    ksp("com.github.bumptech.glide:ksp:4.16.0") // TAMBAHKAN INI
 
     implementation("com.google.firebase:firebase-crashlytics")
     implementation("com.google.firebase:firebase-analytics")
@@ -130,27 +120,24 @@ dependencies {
 
     // DataStore
     implementation("androidx.datastore:datastore-preferences:1.1.4")
-    implementation ("androidx.datastore:datastore-preferences-core:1.1.4")
-
+    implementation("androidx.datastore:datastore-preferences-core:1.1.4")
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
 
     // ViewModel & LiveData
-    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
-    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.8.7")
-    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
 
     // Gson
-    implementation ("com.google.code.gson:gson:2.13.1")
+    implementation("com.google.code.gson:gson:2.13.1")
 
     // Retrofit
-    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
-    implementation ("com.midtrans:uikit:2.3.0-SANDBOX")
-    implementation ("com.midtrans:uikit:2.3.0")
-
+    implementation("com.midtrans:uikit:2.3.0-SANDBOX")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
